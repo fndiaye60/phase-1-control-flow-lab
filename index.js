bb83b81311eed257c43594ebfb0e2d93b442e7bd
farida ndiaye <faridandiaye60@gmail.com>
2023-03-30 22:46:12 -0400

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let message;
+  if (distance<=400){
+    message = "This one is on me!";
+  }else if (distance >40 && distance <=2000){
+    message = "That will be twenty bucks.";
+  }else if (distance >2000 && distance<=2500){
+    message = "I will gladly take your thirty bucks.";
+  }else if (distance>=2501){
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const message = city == "NYC" ? ("Ok, sounds good.") : ("No go.");
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+  switch(tip){
+    case "generous":
+      message = "Thank you so much.";
+      break;
+      case "not as generous":
+        message = "Thank you.";
+        break;
+        default:
+          message = "Bye.";
+          break;
+  }
+  return message;
 }
